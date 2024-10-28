f6ce727cb762fb7b082052347cd3f6b9b8e9b563
sunflwoer519 <92281084+sunflower519sf@users.noreply.github.com>
2024-10-28 22:55:58 +0800

    Add files via upload

---
+++ test/web/test1.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+    
+)
+
+# demo.launch(share=True)
+demo.launch(server_port=8080)
